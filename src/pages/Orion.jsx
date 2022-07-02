9ce0e43bc40a15d4558536b10c170514c5e9305e
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-02 12:20:36 -0300

    Add fonts, palette, rules, typeScale files to fill component Orion Styleguide page

--- src/pages/Orion.jsx
+++ src/pages/Orion.jsx
@@ -1,15 +1,16 @@
 import headersConfig from '../docs/utils/headers.styleguide'
 import MakePreface from '../docs/utils/preface.styleguide'
 
-import paletteCSS from '../docs/themes/demo/palette.css'
-import fontsCSS from '../docs/themes/demo/fonts.css'
-import typeScaleCSS from '../docs/themes/demo/typeScale.css'
-import rulesCSS from '../docs/themes/demo/rules.css'
+import paletteCSS from '../docs/themes/orion/palette.css'
+import fontsCSS from '../docs/themes/orion/fonts.css'
+import typeScaleCSS from '../docs/themes/orion/typeScale.css'
+import rulesCSS from '../docs/themes/orion/rules.css'
 import spacingCSS from '../docs/themes/demo/spacing.css'
 import radiusCSS from '../docs/themes/demo/radius.css'
 
 import Styleguide from './Styleguide'
 
+import '../css/fonts/orion.fonts.css'
 
 function Orion() {
     const prefaceAttributes = MakePreface("orion")
