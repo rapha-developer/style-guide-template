fe96ea2508a9d56683a9b7e94b0e68de73cbe72d
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-26 22:59:05 -0300

    Add Page component (for example to entire page)

---
+++ src/pages/Page.jsx
@@ -0,0 +1,30 @@
+
+import paletteCSS from '../css/demo/palette.css'
+import fontsCSS from '../css/demo/fonts.css'
+import radiusCSS from '../css/demo/radius.css'
+import spacingCSS from '../css/demo/spacing.css'
+import typeScaleCSS from '../css/demo/typography.css'
+
+import MakePreface from '../api/preface.styleguide'
+import headersConfig from '../api/headers.styleguide'
+import Styleguide from './Styleguide'
+
+import '../css/fonts/demo.fonts.css'
+
+function Page(props) {
+    const prefaceAttributes = MakePreface()
+
+    return (
+        <Styleguide
+            preface={prefaceAttributes}
+            headers={headersConfig} 
+            palettes={paletteCSS}
+            fonts={fontsCSS}
+            typeScale={typeScaleCSS}
+            spacing={spacingCSS}
+            radius={radiusCSS}
+        />
+    )
+}
+
+export default Page
