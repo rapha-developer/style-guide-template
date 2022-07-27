abfafd9814c26c2902ac7b5acea963fd2218ec9a
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-26 22:59:40 -0300

    All unnecessary files

--- src/docs/utils/preface.styleguide.js
+++
@@ -1,9 +0,0 @@
-
-export default function MakePreface(subtitle, title) {
-    const isSubtitleValid = (subtitle) ? subtitle : "Maker"
-    const isStyleguideValid = (title) ? title : "styleguide" 
-    return {
-        subtitle: isSubtitleValid,
-        title: isStyleguideValid
-    }
-}
