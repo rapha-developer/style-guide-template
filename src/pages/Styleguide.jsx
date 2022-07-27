4845d38b5f093b86066825474a815c1db3a776d7
rapha-desenvolvedor <contatosraphaelmoreira@gmail.com>
2022-07-26 22:53:28 -0300

    Simplifies the component Styleguide

--- src/pages/Styleguide.jsx
+++ src/pages/Styleguide.jsx
@@ -1,39 +1,55 @@
-import Header from "../components/Header"
-import Preface from "../components/Preface"
-import FeaturesContainer from "../molecules/FeaturesContainer"
-import PaletteContainer from "../molecules/PaletteContainer"
-import TypographyContainer from "../molecules/TypographyContainer"
-import RulesContainer from "../molecules/RulesContainer"
-import weights from '../docs/utils/weights.styleguide'
+import Header from "../components/Header";
+import Preface from "../components/Preface";
+
+import Extract from '../Helpers/Extract'
+import FormattingPalette from "../Helpers/Utils/FormattingPalette";
+import FormattingFonts from "../Helpers/Utils/FormattingFonts";
+import FormattingTypeScale from "../Helpers/Utils/FormattingTypeScale";
+import FormattingRadius from "../Helpers/Utils/FormattingRadius";
+import FormattingSpacing from "../Helpers/Utils/FormattingSpacing";
+
+import FeaturesContainer from "../molecules/FeaturesContainer";
+import PaletteContainer from "../molecules/PaletteContainer";
+import TypographyContainer from '../molecules/TypographyContainer'
 
 function Styleguide(props) {
+    const palettes = new Extract(props.palettes)
+    const fonts = new Extract(props.fonts)
+    const radius = new Extract(props.radius)
+    const spacing = new Extract(props.spacing)
+    const typeScale = new Extract(props.typeScale)
+
+    const formattingPalette = new FormattingPalette(palettes)
+    const formattingTypography = new FormattingTypeScale(typeScale)
+    const formattingFonts = new FormattingFonts(fonts)
+    const formattingRadius = new FormattingRadius(radius)
+    const formattingSpaces = new FormattingSpacing(spacing) 
+
     return (
         <div className="styleguide">
             <Preface
-                subtitle={props.preface.subtitle}  
-                title={props.preface.title}
+                subtitle={props.preface.subtitle} 
+                title={props.preface.title} 
             />
-            
             {props.headers.color && <Header title={props.headers.color} /> }
-            <PaletteContainer styleguide={props.palettes} />
+            <PaletteContainer 
+                palettes={formattingPalette.palettes}
+            />
 
             {props.headers.typography && <Header title={props.headers.typography} /> }
             <TypographyContainer 
-                styleguideFonts={props.fonts}
-                styleguideTypeScale={props.typeScale}
-                weights={weights}
+                fonts={formattingFonts.fonts}
+                typographies={formattingTypography.typographies}
             />
-            <RulesContainer styleguide={props.rules}
-                            weights={weights.data}
-                            headlineText={props.headers.headlines.rules}
-            />
+
+            <div style={{marginBlock: "6rem"}}></div>
 
             {props.headers.spacing && <Header title={props.headers.spacing} /> }
             <FeaturesContainer 
-                topics={props.headers.featuresTopics}
-                styleguideSpaces={props.spacing}
-                styleguideRadius={props.radius}
+                radius={formattingRadius.radius}
+                spaces={formattingSpaces.spacing}
             />
+
             {props.headers.component && <Header title={props.headers.component} /> }
 
         </div>
